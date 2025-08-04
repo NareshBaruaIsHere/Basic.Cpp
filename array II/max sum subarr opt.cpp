afcc937b72b2e18af2c8e010d2a0bcc0da913abd
NareshBaruaIsHere <125339144+NareshBaruaIsHere@users.noreply.github.com>
2025-08-05 00:53:23 +0600

    array 2

---
+++ array II/max sum subarr opt.cpp
@@ -0,0 +1,50 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+void maxSubarraySum1(int *arr, int n)
+{
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            int currSum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                currSum += arr[i];
+            }
+            cout << currSum << ",";
+            maxSum = max(maxSum, currSum);
+            // max(a, max(b,c));
+        }
+        cout << endl;
+    }
+
+    cout << "maximum subarray sum = " << maxSum << endl;
+}
+
+void maxSubarraySum2(int *arr, int n)
+{
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        int currSum = 0;
+        for (int end = start; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+
+    cout << "maximum subarray sum = " << maxSum << endl;
+}
+
+int main()
+{
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    maxSubarraySum2(arr, n);
+    return 0;
+}
